b5a7d9b4bf6cb0bb4f7f49779fe684c60efc4605
anthobldr <anthony.lutard33@gmail.com>
2026-06-09 08:59:30 +0200

    feat(edit): new property for the hero components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,29 @@ import Prestations from "./components/accueil/Prestations";
 import Propos from "./components/accueil/Propos";
 import Instagram from "./components/accueil/Instagram";
 import Avis from "./layouts/Avis";
+import heroStyle from "@/app/layouts/Hero.module.css"
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero
+        title={
+            <>
+            RÉVÉLER VOTRE{" "}
+            <span className={heroStyle.heroEnLight}>Histoire,</span>
+            <br />
+            SUBLIMER VOTRE{" "}
+            <span className={heroStyle.heroEnLight}>Image.</span>
+            </>
+        }
+        subtitle={
+            <>
+            Laissez-vous surprendre par votre propre beauté à travers
+            un regard bienveillant, à la frontière entre authenticité
+            et poésie lumineuse.
+            </>
+        }
+        />
       <Partenaire />
       <Bento />
       <Prestations />
--- src/app/prestations/page.tsx
+++ src/app/prestations/page.tsx
@@ -1,11 +1,28 @@
 import Assurance from "../components/Prestations/Assurance";
 import Avis from "../layouts/Avis";
 import Hero from "../layouts/Hero";
-
+import style from "@/app/layouts/Hero.module.css"
 export default function Prestations(){
     return (
         <>
-        <Hero />
+        <Hero
+        title={
+            <>
+            VOS{" "}
+            <span className={style.heroEnLight}>Moments</span>
+            <br />
+            LES PLUS{" "}
+            <span className={style.heroEnLight}>Précieux</span>
+            </>
+        }
+        subtitle={
+            <>
+            Laissez-vous surprendre par votre propre beauté à travers
+            un regard bienveillant, à la frontière entre authenticité
+            et poésie lumineuse.
+            </>
+        }
+        />
         <Assurance />
         
         <Avis />
